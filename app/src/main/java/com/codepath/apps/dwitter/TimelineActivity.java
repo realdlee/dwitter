cd2a46a6212f6112a6181ed21b8753f23711f9cf
David Lee <ambitiouspear@gmail.com>
2016-08-06 14:46:19 -0700

    Revert "Reorganize activities to activities package"
    This reverts commit d331b45140963d809dc7c4f998511fa5d824d561.

---
+++ app/src/main/java/com/codepath/apps/dwitter/TimelineActivity.java
@@ -0,0 +1,61 @@
+package com.codepath.apps.dwitter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.codepath.apps.dwitter.models.EndlessScrollListener;
+import com.codepath.apps.dwitter.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+    private TwitterClient client;
+    private ArrayList<Tweet> tweets;
+    private TweetsArrayAdapter aTweets;
+    private ListView lvTweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        //create arraylist (data source)
+        tweets = new ArrayList<>();
+        //construct the adapter from data source
+        aTweets = new TweetsArrayAdapter(this, tweets);
+        //connect adapter to list view
+        lvTweets.setAdapter(aTweets);
+        client = TwitterApplication.getRestClient();
+        populateTimeline();
+
+        lvTweets.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                populateTimeline();
+                return true;
+            }
+        });
+    }
+
+    public void populateTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                aTweets.addAll(Tweet.fromJSONArray(response));
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+}
